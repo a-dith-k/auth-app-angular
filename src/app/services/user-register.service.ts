b2fe33dd4fd35dfd3240ed978dc984ce0e89ac84
a-dith-k <adithk1308@gmail.com>
2023-12-25 12:02:38 +0530

    base url updated ,form submit button updated,content-type updated converted request json to string

--- src/app/services/user-register.service.ts
+++ src/app/services/user-register.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 import {RegisterRequest} from "../pages/signup/signup.component";
+import {Observable} from "rxjs";
 
 
 @Injectable({
@@ -8,17 +9,19 @@ import {RegisterRequest} from "../pages/signup/signup.component";
 })
 export class UserRegisterService {
 
-  private url="";
+  private url="http://localhost:8080/auth/register";
 
   private  options={
     headers:{
-      contentType: "application/json"
+      'content-Type': "application/json"
     }
   }
 
   constructor(private http:HttpClient) { }
 
   register(request:RegisterRequest) {
-    this.http.post(this.url,request).subscribe()
+    this.http.post(this.url,JSON.stringify(request),this.options).subscribe(response=>{
+      console.log(response)
+    })
   }
 }
